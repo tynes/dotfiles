b712ebc905177997e8d31c1ca5e3460308643eba
mark tyneway <mark@purse.io>
2018-04-09 21:59:00 -0700

    Autocomplete ssh

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -88,6 +88,9 @@ if type _git &> /dev/null && [ -f `dirname $DIR`/git-completion.bash ]; then
   complete -o default -o nospace -F _git g;
 fi;
 
+# Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
+[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh;
+
 # share current directory on port 8000
 alias webshare='python -m SimpleHTTPServer'
 
