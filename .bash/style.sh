8e63bf936dcc6608f013cd95f3eb0bd54e4ed94f
mark tyneway <mark@purse.io>
2018-04-09 21:58:23 -0700

    Fix ps1 to update live

--- .bash/style.sh
+++ .bash/style.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-export PS1="\n\u \l [exit \$?] \t\n\w\[$(tput sgr0)\]$(parse_git_branch)\n$ "
+function ps1_git_hash_short() {
+    git rev-parse --short HEAD 2>/dev/null | xargs printf "[%s]"
+}
+
+function ps1_parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+export PS1="\n\u \l [exit \$?] \t\n\w\[\$(tput sgr0)\]\$(ps1_parse_git_branch) \$(ps1_git_hash_short)\n$ "
