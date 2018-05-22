6241b8ab465d422ce56f98318428a23feb5d3ee3
mark tyneway <mark@purse.io>
2018-05-22 14:33:04 -0700

    Add ledger signing support and less(1) config

--- .bash/env.sh
+++ .bash/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT_DIR=`dirname $DIR`
+ROOT_DIR=$(dirname $DIR)
 
 # TODO - does this override native things?
 export VIM_RUNTIME="$ROOT_DIR/vim_runtime"
@@ -9,3 +9,23 @@ export VIM_RUNTIME="$ROOT_DIR/vim_runtime"
 # maybe a mac only thing?
 export PATH="$PATH:/usr/local/sbin"
 
+# ledger pgp signing
+# TODO: if HOME/Library exists, add this to PATH
+export PATH="$PATH/:$HOME/Library/Python/3.6/bin"
+
+# default to using ledger
+export GNUPGHOME=~/.gnupg/ledger
+
+function gpghome() {
+    selection=$(echo "$HOME/.gnupg/ledger
+$HOME/.gnupg" | fzf)
+    export GNUPGHOME="$selection"
+}
+
+# less options
+export LESS='-R -C -M -I -j 10 -# 4'
+# -C - make full screen reprints faster
+# -M - display more info in status line
+# -I - ignore casing in search
+# -j 10 - display search results in line 10
+# -# 4 - move 4 characters left/right on arrow key press
