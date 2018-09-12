b127c89171d8155b7df4ad4de43668adae9e0a92
mark tyneway <mark@purse.io>
2018-09-11 19:21:05 -0700

    feature: random functions

---
+++ .bash/random.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+function random_string() {
+    LC_CTYPE=C tr -dc '0-9a-z' </dev/urandom | head -c"$1"
+}
+
+function random_bytes() {
+    hexdump -n "$1" -e '4/4 "%08x"' /dev/random
+}
