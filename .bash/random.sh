9347c0c366e37ebc2f1f7985761f2994e14bf948
Mark Tyneway <mark.tyneway@gmail.com>
2026-01-12 16:06:46 -0800

    bash: delete dead code

--- .bash/random.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-function random_string() {
-    LC_CTYPE=C tr -dc '0-9a-z' </dev/urandom | head -c"$1"
-}
-
-function random_bytes() {
-    hexdump -n "$1" -e '4/4 "%08x"' /dev/random
-}
