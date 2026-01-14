835d24b5cd33989ab441a28756bebe4a0fec739d
Mark Tyneway <mark.tyneway@proton.me>
2026-01-14 10:59:23 -0800

    bash: delete out of date go config

--- .bash/go.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if command -v go &> /dev/null
-then
-    export GO111MODULE=on
-    export GOROOT=$(go env GOROOT)
-    export GOPATH=$HOME/go
-    PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
-fi
