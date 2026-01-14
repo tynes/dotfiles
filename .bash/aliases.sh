fc356e334ac80349212301825aa1e148ba6cecaa
Mark Tyneway <mark.tyneway@proton.me>
2026-01-14 10:52:08 -0800

    aliases: cleanup

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -24,48 +24,16 @@ alias lla='ls -la'
 alias l='ls'
 alias ..='cd ..'
 alias c='clear'
+function mkcd() { mkdir -p "$@" && cd "$_"; }
+function cdls() { cd "$@" && ls }
 alias z='zoxide'
 
 # neovim
 alias e='nvim'
 
-# move back up one directory and ls
-function ..l() {
-    cd .. && ls "$@"
-}
+alias diff='difft'
 
-function u() {
-    [  "$#" -ne 1 ] && echo "usage: u [int] - traverse directories upwards"
-    if [[ "$1" -gt 0 ]]; then
-        for i in $(seq "$@"); do
-            cd ..
-        done
-    fi
-}
-
-# mkdir and cd
-function mkcd() { mkdir -p "$@" && cd "$_"; }
-
-# cd and ls
-function cdls() {
-    cd "$@" && ls
-}
-
-# TODO: put this behind darwin flag
-# use homebrew git
-if which brew &>/dev/null; then
-    alias git="`brew --prefix`/bin/git"
-fi
-
-# docker stuff
-alias d='docker'
-alias di='docker images'
-alias dps='docker ps'
-alias dc='docker-compose'
-alias dclean="docker images -f 'dangling=true' | xargs docker rmi"
-alias dcleans="docker ps -a --format='{{.ID}}' | xargs docker rm"
-
-# git stuff
+# git
 alias g='git'
 alias gs='git status'
 alias gp='git pull'
@@ -74,20 +42,19 @@ alias gpom='git pull origin master'
 alias ga='git add'
 alias gc='git commit'
 alias gco='git checkout'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset \
+-%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
-alias prettypath='tr ":" "\n" <<< "$PATH"'
-
-# Enable tab completion for `g` by marking it as an alias for `git`
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if type _git &> /dev/null && [ -f `dirname $DIR`/git-completion.bash ]; then
-  complete -o default -o nospace -F _git g;
-fi;
+# docker
+alias d='docker'
+alias di='docker images'
+alias dps='docker ps'
+alias dc='docker-compose'
+alias dclean="docker images -f 'dangling=true' | xargs docker rmi"
+alias dcleans="docker ps -a --format='{{.ID}}' | xargs docker rm"
 
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh;
-
-alias gl="git log --graph --pretty=format:'%Cred%h%Creset \
--%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 # kubernetes stuff
 alias k='kubectl'
