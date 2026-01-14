0af3380b2f7c1292bdcd176ac2252024891898f2
Mark Tyneway <mark.tyneway@proton.me>
2026-01-14 10:46:47 -0800

    history: update

--- .bash/history.sh
+++ .bash/history.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
 alias hist='history'
-# Avoid duplicates
-export HISTCONTROL=ignoredups:erasedups
 
-# append to the history file instead of overwriting
-# it when shell exits
+# Don't save commands prefixed with a space, and remove all duplicate entries.
+# `ignoreboth` is a shortcut for `ignorespace:ignoredups`.
+export HISTCONTROL=ignoreboth:erasedups
+
+# Ignore common, trivial commands.
+export HISTIGNORE="ls:cd:bg:fg:history:exit"
+
+# Add timestamps to history, e.g., "2026-01-14 15:30:00".
+export HISTTIMEFORMAT="%F %T "
+
+# Append to the history file instead of overwriting it when the shell exits.
 shopt -s histappend
-# large history file size
+
+# Use a large history file size.
 export HISTSIZE=100000
 export HISTFILESIZE=100000
 
-# After each command, append to the history file and reread it
-export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+# After each command, append to the history file and reread it to share
+# history between running terminals.
+export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'
+'}history -a; history -c; history -r"
